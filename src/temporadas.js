ef9ba1684b1d133638d59f2825b59f675bcbc164
dasotosilva19 <dasoto1@uc.cl>
2021-04-06 16:02:47 -0400

    primer commit

---
+++ src/temporadas.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Temporadas = () => (
+  /*fetch('https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=Breaking+Bad')
+            .then(response => response.json())
+            .then(temporadasBBJson => {this.setState(
+                { breakingBadTemporadas: temporadasBBJson, loading: false, }, 
+                () => console.log(this.state))})*/
+  <p>lol</p>
+  
+)
